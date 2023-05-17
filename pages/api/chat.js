64928e0f1fdadda768a2c44271efd543933a0e62
Aditya Raj Kumawat <arkumawat78@gmail.com>
2023-05-18 04:02:39 +0530

    ok

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -81,9 +81,9 @@ export default async function (req, res) {
       res.write(chunkData.token);
     }
 
-    res.status(200).end("END_STREAM");
+    return res.status(200).end("END_STREAM");
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: error.message });
+    return res.status(500).end("END_STREAM");
   }
 }
